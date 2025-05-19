4277928201b7b23c4dbb4fd707b3c300f469f5cb
Felix Berger <felix.enok.berger@gmail.com>
2025-05-19 20:22:35 +0000

    Added shadcn

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@auxarmory/ui/components/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -8,6 +9,9 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button variant="default" className="mt-2">
+        Click me!
+      </Button>
     </div>
   )
 }

25eee8816d729e5a42bc3c4bd58c72d11d363d2b
Felix Berger <felix.enok.berger@gmail.com>
2025-06-01 11:07:23 +0000

    Added typing to PlayableClassIndex

--- packages/battlenet/src/wow/game_data/playable_class.ts
+++ packages/battlenet/src/wow/game_data/playable_class.ts
@@ -1,12 +1,17 @@
 import { z } from "zod/v4";
 
 import type { WoWGameDataClient } from "..";
+import { LinkSelfResponse, KeyNameIdResponse } from "../../types";
+
+export const PlayableClassIndexResponse = LinkSelfResponse.extend({
+	classes: z.array(KeyNameIdResponse),
+});
 
 export function PlayableClassIndex(this: WoWGameDataClient) {
-	return this.request<unknown>({
+	return this.request<z.infer<typeof PlayableClassIndexResponse>>({
 		endpoint: `data/wow/playable-class/index`,
 		namespace: "static",
-		zod: z.unknown(),
+		zod: PlayableClassIndexResponse,
 	});
 }
 

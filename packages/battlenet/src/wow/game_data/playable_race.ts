b26f55e7806398bf38797806bc8029be4046d532
Felix Berger <felix.enok.berger@gmail.com>
2025-06-01 12:08:41 +0000

    Added typing to playable races

--- packages/battlenet/src/wow/game_data/playable_race.ts
+++ packages/battlenet/src/wow/game_data/playable_race.ts
@@ -1,12 +1,17 @@
 import { z } from "zod/v4";
 
 import type { WoWGameDataClient } from "..";
+import { LinkSelfResponse, KeyNameIdResponse } from "../../types";
+
+export const PlayableRaceIndexResponse = LinkSelfResponse.extend({
+	races: z.array(KeyNameIdResponse),
+});
 
 export function PlayableRaceIndex(this: WoWGameDataClient) {
-	return this.request<unknown>({
+	return this.request<z.infer<typeof PlayableRaceIndexResponse>>({
 		endpoint: `data/wow/playable-race/index`,
 		namespace: "static",
-		zod: z.unknown(),
+		zod: PlayableRaceIndexResponse,
 	});
 }
 

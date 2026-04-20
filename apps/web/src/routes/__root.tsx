9509571b9c4060f90809806f3016d5695ae589ad
aske <aske020304@gmail.com>
2026-04-20 14:29:47 +0200

    fix: removed tanstack hotkey that caused issue with input fields

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -152,6 +152,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<TanStackDevtools
 					config={{
 						position: 'bottom-right',
+						openHotkey: [],
 					}}
 					plugins={[
 						{
